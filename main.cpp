20a10ed1f1ec45c18b41e1e2ca856604cb8f7cd1
Earth <85964403+EarthRulerr@users.noreply.github.com>
2022-11-09 09:18:41 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+using namespace std;
+
+int main() {
+  system("clear");
+
+  // code
+
+}
